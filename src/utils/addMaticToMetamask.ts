25b53104986cfd125c8a0a8005946ebe5d60d38f
jedinrk <jedi.nrk@gmail.com>
2023-03-25 18:46:31 +0400

    Fixed Network switch to testnet

--- src/utils/addMaticToMetamask.ts
+++ src/utils/addMaticToMetamask.ts
@@ -3,29 +3,39 @@ const addMaticToMetamask: () => void = () => {
   if (ethereum) {
     ethereum
       .request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: '0x89',
-            chainName: 'Polygon Network',
-            rpcUrls: ['https://polygon-rpc.com/'],
-            iconUrls: [
-              'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png',
-            ],
-            blockExplorerUrls: ['https://polygonscan.com/'],
-            nativeCurrency: {
-              name: 'Matic Token',
-              symbol: 'MATIC',
-              decimals: 18,
-            },
-          },
-        ], // you must have access to the specified account
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x13881' }],
       })
-      .catch((error: any) => {
-        if (error.code === 4001) {
-          console.log('We can encrypt anything without the key.');
-        } else {
-          console.error(error);
+      .catch((err: any) => {
+        // This error code indicates that the chain has not been added to MetaMask
+        if (err.code === 4902) {
+          ethereum
+            .request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainId: '0x13881',
+                  chainName: 'Mumbai Testnet',
+                  rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+                  iconUrls: [
+                    'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png',
+                  ],
+                  blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
+                  nativeCurrency: {
+                    name: 'Matic Token',
+                    symbol: 'MATIC',
+                    decimals: 18,
+                  },
+                },
+              ], // you must have access to the specified account
+            })
+            .catch((error: any) => {
+              if (error.code === 4001) {
+                console.log('We can encrypt anything without the key.');
+              } else {
+                console.error(error);
+              }
+            });
         }
       });
   }
